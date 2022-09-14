b588ad9c47e11b884603da18cd8a41d3c8d8ff0f
phongtranslator <chienpr123@gmail.com>
2022-09-14 12:49:11 +0700

    update

--- js/app.js
+++ js/app.js
@@ -24,8 +24,8 @@
         i = [];
 
     function a(t) {
-        console.log(c.p);
-        return "../atweb/js/" + ({} [t] || t) + "." + {
+        console.log(c);
+        return c.p + "atweb/js/" + ({} [t] || t) + "." + {
             "chunk-0b65ffb6": "33822ef0",
             "chunk-12950967": "efd128fb",
             "chunk-2d0a3577": "d9f896b2",
